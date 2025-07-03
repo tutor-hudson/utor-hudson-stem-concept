853198ae570da23a132efd9f64cefe7f50c28ddf
tutor-hudson <hudsonkamau189@gmail.com>
2025-07-03 14:13:23 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
 
-// Your Firebase config here
 const firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "SENDER-ID",
-  appId: "APP-ID"
+  apiKey: "AIzaSyAn0roGODpezHlADas9LSJ9ujNv3bLN8CY",
+  authDomain: "tutor-hudson-c2737.firebaseapp.com",
+  projectId: "tutor-hudson-c2737",
+  storageBucket: "tutor-hudson-c2737.firebasestorage.app",
+  messagingSenderId: "61009929913",
+  appId: "1:61009929913:web:305cbd7b0d6eeb30b69d68"
 };
-
 firebase.initializeApp(firebaseConfig);
