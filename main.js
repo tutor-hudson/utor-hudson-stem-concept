e61d9682dcd1029037724fc86f74aa94783a0e13
tutor-hudson <hudsonkamau189@gmail.com>
2025-07-03 12:49:08 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function login() {
+  alert('Login function to be connected to Firebase');
+}
+
+function signup() {
+  alert('Sign-up function to be connected to Firebase');
+}
+
+function logout() {
+  alert('Logout function to be connected to Firebase');
+}
+
+function uploadFile() {
+  alert('File upload function to be implemented with Firebase Storage');
+}
